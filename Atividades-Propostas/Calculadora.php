510ab2911d5215469fe9c9d201a27b28f5b6bfb3
Giovanna Stypurski <stypurskigiovanna@gmail.com>
2026-08-22 11:39:52 +0000

    salvar alteracoes do codigo

---
+++ Atividades-Propostas/Calculadora.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang ="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Calculadora</title> 
+</head>
+<body>
+    <form action="Calculadora.php" method = "GET" name = "Calculadora">
+        x: <input type:"number" name:"x"><br>
+		sinal: <input type:"text" name:"sinal"><br>
+		y: <input type:"number" name:"y"><br>
+		<input type:"submit" value:"enviar">
+    </form>
+</body>
+</html>
+
+
+<?php
+
+$valor1 = $_GET["x"];
+$valor2 = $_GET["y"];
+$sinal = $_GET["sinal"];
+
+switch($sinal){
+    case "+":
+		$final = $valor1 + $valor2;
+		break;
+	
+	case "-":
+		$final = $valor1 - $valor2;
+		break;
+		
+	case "*":
+		$final = $valor1 * $valor2;
+		break;
+		
+	case "/":
+		$final = $valor1 / $valor2;
+		break;
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Calculadora</title> 
+	</head> 
+	<body>
+		<?php echo "<h1>Resultado: $final </h1>"; ?>
+	</body>
+	</html>
+
